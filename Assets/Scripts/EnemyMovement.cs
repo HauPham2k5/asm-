2c1f1257780e5618de85419b4acfe1352cb6e44e
HauPham2k5 <hauphpd10095@gmail.com>
2024-06-14 14:23:21 +0700

    first commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float moveDistance = 3f; // Khoảng cách di chuyển
+    private Vector2 startingPosition; // Vị trí bắt đầu của quái vật
+    private Rigidbody2D myRigidbody;
+    private bool movingRight = true;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position; // Lưu lại vị trí bắt đầu
+    }
+
+    void Update()
+    {
+        MoveBackAndForth();
+    }
+
+    void MoveBackAndForth()
+    {
+        if (movingRight)
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+            if (Vector2.Distance(startingPosition, transform.position) >= moveDistance)
+            {
+                movingRight = false;
+                FlipDirection();
+            }
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(-moveSpeed, myRigidbody.velocity.y);
+            if (Vector2.Distance(startingPosition, transform.position) <= 0)
+            {
+                movingRight = true;
+                FlipDirection();
+            }
+        }
+    }
+
+    void FlipDirection()
+    {
+        // Đổi hướng hiển thị của sprite
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] int maxJumps = 2;
+    [SerializeField] GameObject arrowPrefab; // Prefab đạn
+    [SerializeField] Transform arrowSpawnPoint; // Điểm xuất hiện đạn
+    [SerializeField] float arrowSpeed = 10f; // Tốc độ đạn
+
+    Vector2 moveInput;
+    Rigidbody2D myRi;
+    Animator myA;
+    bool isGrounded;
+    int jumpCount; // Số lần nhảy
+
+    void Start()
+    {
+        myRi = GetComponent<Rigidbody2D>();
+        myA = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+        CheckGrounded();
+    }
+
+    // Xử lý sự kiện di chuyển từ Input System
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    // Xử lý sự kiện nhảy từ Input System
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed && (isGrounded || jumpCount < maxJumps))
+        {
+            Jump();
+        }
+    }
+    void OnFire(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            FireArrow();
+        }
+    }
+
+
+    // Hàm điều khiển chạy của nhân vật
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRi.velocity.y);
+        myRi.velocity = playerVelocity;
+        bool hasHorizontalSpeed = Mathf.Abs(myRi.velocity.x) > Mathf.Epsilon;
+        myA.SetBool("isRunning", hasHorizontalSpeed);
+    }
+
+    // Hàm đảo hướng sprite của nhân vật
+    void FlipSprite()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(myRi.velocity.x) > Mathf.Epsilon;
+        if (hasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRi.velocity.x), 1f);
+        }
+    }
+
+    // Hàm xử lý nhảy của nhân vật
+    void Jump()
+    {
+        if (jumpCount == 0)
+        {
+            myA.SetBool("isDoubleJump", false);
+        }
+        else if (jumpCount == 1)
+        {
+            myA.SetBool("isDoubleJump", true);
+        }
+
+        myRi.velocity = new Vector2(myRi.velocity.x, jumpForce);
+        jumpCount++;
+    }
+
+    // Hàm kiểm tra nhân vật có đang chạm đất không
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        myA.SetBool("isGrounded", isGrounded);
+
+        if (isGrounded)
+        {
+            jumpCount = 0;
+            myA.SetBool("isDoubleJump", false);
+        }
+    }
+    void FireArrow()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, Quaternion.identity);
+        Rigidbody2D arrowRb = arrow.GetComponent<Rigidbody2D>();
+
+        if (arrowRb != null)
+        {
+            float direction = transform.localScale.x; // Xác định hướng nhân vật đang đối diện
+            arrowRb.velocity = new Vector2(direction * arrowSpeed, 0f);
+        }
+    }
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Poir"))
+        {
+            transform.position = arrowSpawnPoint.position; // Quay lại vị trí xuất phát
+            myRi.velocity = Vector2.zero; // Reset vận tốc
+            Debug.Log("Player hit a monster and returned to spawn point.");
+        }
+    }
+
+}
